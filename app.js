8ff028e782a0afe7109001e15dba5e6bfba5f2bd
Jyothi Perla <jyothiharathi3@gmail.com>
2021-07-21 14:24:35 +0000

    import and export today's date

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  let date = new Date();
+  response.send(
+    `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  );
+});
+
+//app.listen(3000);
+module.exports = app;
